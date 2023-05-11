dcb5a0731a2796322a422a06a8b5fd934107337d
sziben <100142081+sziben@users.noreply.github.com>
2023-05-11 19:37:21 +0200

    first commit

---
+++ Conditionals/main.py
@@ -0,0 +1,14 @@
+szam1 = 40
+szam2 = 40
+
+#Elszererném dönteni, hogy szam1 nagyobb-e mint szam2
+
+if szam1 == szam2:
+    print("szam1 nagyobb")
+else:
+    print("szam1 nem nagyobb")
+    print("Ez biztos, hogy lefut")
+    print("Ez biztos, hogy lefut")
+    print("Ez biztos, hogy lefut")
+    print("Ez biztos, hogy lefut") #indentation
+print("Ez biztos, hogy lefut")
